73a7f67f4a34aaf360286f4a861a609ad80130b3
Sarah Weaver <9245433+LWFlouisa@users.noreply.github.com>
2024-04-11 08:22:55 +0000

    Add files via upload

---
+++ _brains/second_color_cymk.rb
@@ -0,0 +1,11 @@
+require "rexml/document"
+include REXML
+
+palletier_file = File.new("data/second_container.xml")
+palletier_doc  = Document.new(palletier_file)
+
+# Get root elements.
+root = palletier_doc.root
+
+# This will output the text of the user introduction.
+palletier_doc.elements.each("screen/container/color/color_cymk") { |e| print e.text + " " }
